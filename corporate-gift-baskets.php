6e7b525c0f50ce82cfb397c9c60515d885b58bde
John Barrett <johnjbarrett22@gmail.com>
2022-09-07 23:25:18 -0700

    Adding the files I have worked on or made changes to

---
+++ corporate-gift-baskets.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Fancifull Gift Baskets - Los Angeles, Hollywood, California</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Fancifull Gift Baskets">
+
+    <link rel="shortcut icon" href="favicon.png">
+
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="dist/styles/main.css">
+</head>
+<body>
+    <div class="wrapper">
+        <?php include_once('site-header.php');?>
+        <?php include_once('menu15-version2.php');?>
+
+        <main class="container">
+            <div class="hero hero-listing">
+				<div class="category">
+					<h1 class="listing-title">Corporate Gift Baskets</h1>
+                    <blockquote class="ff-quote">Business is personal. We understand this because Personal Service is the foundation of our business. <strong>Here is what we offer that others don't:</strong></blockquote>
+                    <p><strong>Custom Designs Just For You:</strong> From one to thousands we are small enough to be personal but big enough to get the job done.</p>
+                    <p><strong>Send your Branded Items:</strong> We can incorporate them in a design or create a promotional gift just for you.</p>
+                    <p><strong>Custom Containers and Items made for your order:</strong> We can make Boxes with your logo as well as Crates, Printed Ribbon, Branded Cutting Boards - we do it all.</p>
+                    <p><strong>Delivery and Shipping throughout the U.S.:</strong> Send your list over and we take care of all the details.</p>
+                    <p><strong>Fast Turnaround Time:</strong> We can often deliver same day in Los Angeles. Call early!</p>
+                    <p><strong>One Stop Shop:</strong> From Fine Wine to Fresh Flowers, we are the Gifting Experts.</p>
+                    <p><a href="tel:+3234667654">Call 323-466-7654</a> or <a href="mailto:sales@fancifull.com">email.</a></p>
+				</div>
+				<div class="category--image" style="height: auto;background-image: url(images/custom_corporate_branding.jpg);"></div>
+			</div>
+
+            <div class="container featured-category">
+
+                <div class="row mobile-flex">
+                    <section class="col-sm-12 col-md-3">
+                        <h2 class="featured-cat-title" id="HR">Human Resources</h2>
+                        <p>Experts agree that it is important to keep employees engaged and feeling appreciated. The most common HR gifts are for: <br>
+                            Babies<br>
+                            Get Well<br>
+                            Condolence<br>
+                        </p>
+                        <p>Some companies also celebrate anniversaries, birthdays and more. No matter the occasion we can design a single gift or a program
+                            for you. Ask us about a custom webpage just for you to make ordering gifts easy.
+                        </p>
+                    </section>
+                    <figure class="grid-basket col-sm-4 col-md-3">
+                        <a href="baby-gift-baskets.php"><img class="img-responsive" src="images/bj7-hcr.jpg"></a>
+                        <figcaption>
+                            <h3 class="category-title">
+                                <a href="baby-gift-baskets.php">Baby Gift Baskets</a>
+                            </h3>
+                            Each of our beautiful baby gift baskets are designed to create a wonderful lasting memory for the proud parents!
+                        </figcaption>
+                    </figure>
+                    <figure class="grid-basket col-sm-4 col-md-3">
+                        <a href="get-well-gift-baskets.php"><img class="img-responsive" src="images/gw-hcr.jpg"></a>
+                        <figcaption>
+                            <h3 class="category-title">
+                                <a href="get-well-gift-baskets.php">Get Well Baskets</a>
+                            </h3>
+                            Send some cheer with flowers or a get well gift basket to show your concern and support.
+                        </figcaption>
+                    </figure>
+                    <figure class="grid-basket col-sm-4 col-md-3">
+                        <a href="condolence-gift-baskets.php"><img class="im-responsive" src="images/frgo9-hcr.jpg"></a>
+                        <figcaption>
+                            <h3 class="category-title">
+                                <a href="condolence-gift-baskets.php">Condolence</a>
+                            </h3>
+                            Express your sympathy.
+                        </figcaption>
+                    </figure>
+                </div>
+
+                <div class="row mobile-flex">
+                    <section class="col-sm-12 col-md-3">
+                        <h2 class="featured-cat-title">Custom Gifts</h2>
+                        <h3>Branding and Promotion</h3>
+                        <p>There are so many reasons to order a custom gift. The most popular are: you have branded items to use,
+                        welcoming clients to a convention, auction and promotional baskets or new product launches</p>
+                        <p>Whatever you need, from a small token to a slew of baskets, we can do it</p>
+                    </section>
+                    <figure class="grid-basket col-sm-4 col-md-3">
+                        <a href="custom-basket-order-page.php"><img class="img-responsive" src="images/night-at-the-museum-custom-basket-hcr.jpg"></a>
+                        <figcaption>
+                            <h3 class="category-title">
+                                <a>You Want What?!</a>
+                            </h3>
+                            Come to us with your wildest ideas. We guarantee you we can make them come true.  <a href="custom-basket-order-page.php">CLICK</a> OR CALL 323.466.7654
+                        </figcaption>
+                    </figure>
+                    <figure class="grid-basket col-sm-4 col-md-3">
+                        <a href="custom-basket-order-page.php"><img class="img-responsive" src="images/bathandbodyworks-hcr.jpg"></a>
+                        <figcaption>
+                            <h3 class="category-title">
+                                <a>Conventions</a>
+                            </h3>
+                            Part of a convention? We can make all the compact gifts you need to make your company stand out. <a href="custom-basket-order-page.php">CLICK</a> OR CALL 323.466.7654
+                        </figcaption>
+                    </figure>
+                    <figure class="grid-basket col-sm-4 col-md-3">
+                        <a><img class="img-responsive" src="images/tapr-hcr.jpg"></a>
+                        <figcaption>
+                            <h3 class="category-title">
+                                <a href="custom-basket-order-page.php">Your Own Items</a>
+                            </h3>
+                            We can create a basket filled with anything you want. <a href="custom-basket-order-page.php">CLICK</a> OR CALL 323.466.7654 or COME ON IN
+                        </figcaption>
+                    </figure>
+                </div>
+
+                <div class="row mobile-flex">
+                    <section class="col-sm-12 col-md-3">
+                        <h2 class="featured-cat-title">Client Appreciation</h2>
+                        <p>Studies show thanking clients will raise your sales and profit margins</p>
+                        <p>Fancifull is the expert. We excel at: year end, closing a big sale and tenant appreciation.</p>
+                        <p>We are your corporate gifting specialists</p>
+                    </section>
+                    <figure class="grid-basket col-sm-4 col-md-3">
+                        <a><img class="img-responsive" src="images/go12-hcr.jpg"></a>
+                        <figcaption>
+                            <h3 class="category-title">
+                                <a>Something For Everyone</a>
+                            </h3>
+                        </figcaption>
+                    </figure>
+                    <figure class="grid-basket col-sm-4 col-md-3">
+                        <a><img class="img-responsive" src="images/warm-wishes-wine-gift-basket-hcr.jpg"></a>
+                        <figcaption>
+                            <h3 class="category-title">
+                                <a>Warm Wishes</a>
+                            </h3>
+                        </figcaption>
+                    </figure>
+                    <figure class="grid-basket col-sm-4 col-md-3">
+                        <a><img class="img-responsive" src="images/gof13-hcr.jpg"></a>
+                        <figcaption>
+                            <h3 class="category-title">
+                                <a>Find Foods & Fresh Fruit</a>
+                            </h3>
+                        </figcaption>
+                    </figure>
+                </div>
+
+
+
+            </div>
+
+        </main>
+        <?php include_once('includes/global/footer.php'); ?>
+
+    <script src="dist/scripts/main.js"></script>
+</body>
+</html>
